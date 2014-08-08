f9fe04b54a535a9c30606910c97a39e8711aae85
Justin Reidy <jmreidy@rzrsharp.net>
2014-08-08 15:21:49 -0700

    [added] initial test coverage

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 
-function buildEntries() {
-  return fs.readdirSync('examples').reduce(function(entries, dir) {
-    if (dir === 'build') {
-      return entries;
-    }
-    var isDraft = dir.charAt(0) === '_';
-    if (!isDraft && fs.lstatSync(path.join('examples', dir)).isDirectory()) {
-      entries[dir] = './examples/'+dir+'/'+'main.js';
-    }
-    return entries;
-  }, {});
-}
-
 module.exports = {
-  entry: buildEntries(),
-
   output: {
     filename: '[name].js',
     chunkFilename: '[id].chunk.js',
